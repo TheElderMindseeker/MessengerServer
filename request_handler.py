b458363c69d52c165aaba7e5491f5ffd2186ff99
niyaz1998 <niyaz1998@yandex.ru>
2017-10-29 18:38:56 +0300

    Improved Login part

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 def request_handler(sock, addr):
     flag_handshaked = False
     user_id = -1
@@ -16,15 +17,16 @@ def request_handler(sock, addr):
             else:
                 issue_error_message(sock, '', addr)
                 break
-        elif request[:5] == 'Login':
-            if request.split(':', maxsplit=1)[0] == 'Login':
-                login = request.split(':', maxsplit=1)[1]
-                cursor.execute("SELECT count(*) FROM users WHERE login_name=?;", (login,))
-                count = cursor.fetchone()
-                if count[0] == 0:
-                    cursor.execute("INSERT INTO users (login_name) VALUES (?);", (login,))
-                    connection.commit()
-                send_by_socket(sock, 'Successful', addr)
+        elif request[:5] == 'Login' and user_id == -1:
+            login = request.split(':', maxsplit=1)[1]
+            cursor.execute("SELECT count(*) FROM users WHERE login_name=?;", (login,))
+            count = cursor.fetchone()
+            if count[0] == 0:
+                cursor.execute("INSERT INTO users (login_name) VALUES (?);", (login,))
+                connection.commit()
+            cursor.execute("SELECT user_id FROM users WHERE login_name=?;", (login,))
+            user_id = cursor.fetchone()
+            send_by_socket(sock, 'Successful', addr)
         elif request == 'Get list of users':
             cursor.execute('''SELECT login_name FROM users;''')
             response = 'Success'
