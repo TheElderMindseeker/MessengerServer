3bc5412ca26d3e7dea0c1a1e00ac8fb875642b66
Daniil Alexandrovich <daniil.botnarenku@gmail.com>
2017-10-31 14:58:24 +0300

    Made all sockets blocking to prevent CPU free-running while waiting

--- server.py
+++ server.py
@@ -8,10 +8,11 @@ if __name__ == "__main__":
 
     server_socket = socket.socket()
     server_socket.bind((HOST, PORT))
-    server_socket.listen(2)
+    server_socket.listen(5)
+    server_socket.setblocking(True)
 
     while True:
-        time.sleep(1)
         socket, address = server_socket.accept()
+        socket.setblocking(True)
         new_thread = threading.Thread(target=request_handler, args=(socket, address))
         new_thread.start()
